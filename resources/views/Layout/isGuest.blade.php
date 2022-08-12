172669064cd29c5441e67a8664032039d64545c7
DeniHidayatullah <denih1360@gmail.com>
2022-08-12 12:16:15 +0700

    push tampilan
    tampilan home dan login menggunakan boostrap

--- resources/views/Layout/isGuest.blade.php
+++ resources/views/Layout/isGuest.blade.php
@@ -5,19 +5,77 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Course Laravel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <style>
+    .divider:after,
+    .divider:before {
+    content: "";
+    flex: 1;
+    height: 1px;
+    background: #eee;
+    }
+    .h-custom {
+    height: calc(100% - 73px);
+    }
+    @media (max-width: 450px) {
+    .h-custom {
+    height: 100%;
+    }
+    }
+  </style>
 </head>
-<body>
-  <div>
-    <header>
-      <nav><a href="/">Home</a></nav>
-      <nav><a href="/login">Login</a></nav>
-    </header>
-    <div>
+<body class="p-3 m-0 border-0 bd-example">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary pb-2 mb-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Course</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
       {{-- our content --}}
       @yield('content')
       {{-- end our content --}}
-    </div>
-    <footer>&copy; Course Laravel Made With </footer>
+    {{-- <footer>&copy; Course Laravel Made With </footer> --}}
+    
+  <div
+  class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+  <!-- Copyright -->
+  <div class="text-white mb-3 mb-md-0">
+    Copyright © 2022. Course Laravel .
   </div>
+  <!-- Copyright -->
+
+  <!-- Right -->
+  <div>
+    <a href="#!" class="text-white me-4">
+      <i class="fab fa-facebook-f"></i>
+    </a>
+    <a href="#!" class="text-white me-4">
+      <i class="fab fa-twitter"></i>
+    </a>
+    <a href="#!" class="text-white me-4">
+      <i class="fab fa-google"></i>
+    </a>
+    <a href="#!" class="text-white">
+      <i class="fab fa-linkedin-in"></i>
+    </a>
+  </div>
+  <!-- Right -->
+</div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,21 @@
 @extends('Layout/isGuest')
 
 @section('content')
+<div class="card-header">
     <h3>{{$title}}</h3>
-    <div>
-        @foreach ($articles as $articles)
-            <div>
-                <h3>{{ $articles->title  }}</h3>
+  </div>
+            <div class="row">
+                @foreach ($articles as $articles)
+                <div class="col-sm-6 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">{{$articles->title}}</h5>
+                        <small class="text-muted">{{$articles->tag}}</small>
+                        <br>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
-            <hr/>
-        @endforeach
-    </div>
 @endsection
